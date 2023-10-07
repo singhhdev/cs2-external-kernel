3b6d85e1df5734e690717858549fab2d80a6c39d
singhhdev <108330780+singhhdev@users.noreply.github.com>
2023-10-07 00:12:04 -0700

    Add files via upload

---
+++ cs2/Valorant/main.cpp
@@ -0,0 +1,174 @@
+#include <Windows.h>
+#include <iostream>
+#include <thread>
+#include <string>
+#include <filesystem>
+#include "driver/driver.hpp"
+#include "overlay/render.hpp"
+#include "overlay/menu.hpp"
+
+#include "game/cheat.hpp"
+
+#include <TlHelp32.h>
+#include <string>
+
+
+DWORD GetProcessID(const std::wstring processName)
+{
+	PROCESSENTRY32 processInfo;
+	processInfo.dwSize = sizeof(processInfo);
+
+	HANDLE processesSnapshot = CreateToolhelp32Snapshot(TH32CS_SNAPPROCESS, NULL);
+	if (processesSnapshot == INVALID_HANDLE_VALUE)
+	{
+		return 0;
+	}
+
+	Process32First(processesSnapshot, &processInfo);
+	if (!processName.compare(processInfo.szExeFile))
+	{
+		CloseHandle(processesSnapshot);
+		return processInfo.th32ProcessID;
+	}
+
+	while (Process32Next(processesSnapshot, &processInfo))
+	{
+		if (!processName.compare(processInfo.szExeFile))
+		{
+			CloseHandle(processesSnapshot);
+			return processInfo.th32ProcessID;
+		}
+	}
+
+	CloseHandle(processesSnapshot);
+	return 0;
+}
+
+int main()
+{
+	printf("> cs2 cheat starting... \n");
+
+	while (Entryhwnd == NULL)
+	{
+		printf("> waiting for counter strike...\r");
+		Sleep(1);
+		processid = GetProcessID(L"cs2.exe");
+		Entryhwnd = get_process_wnd(processid);
+		Sleep(1);
+	}
+
+	driver.initdriver(processid);
+	
+	client = (uintptr_t)driver.client_address();
+	std::cout << "> client: " << std::hex << client << std::endl;
+	
+	setup_window();
+	init_wndparams(MyWnd);
+	main_loop();
+
+	exit(0);
+}
+
+auto render() -> void
+{
+	ImGui_ImplDX9_NewFrame();
+	ImGui_ImplWin32_NewFrame();
+	ImGui::NewFrame();
+
+	espLoop();
+	drawmenu();
+
+	ImGui::EndFrame();
+	p_Device->SetRenderState(D3DRS_ZENABLE, false);
+	p_Device->SetRenderState(D3DRS_ALPHABLENDENABLE, false);
+	p_Device->SetRenderState(D3DRS_SCISSORTESTENABLE, false);
+	p_Device->Clear(0, NULL, D3DCLEAR_TARGET, D3DCOLOR_ARGB(0, 0, 0, 0), 1.0f, 0);
+
+	if (p_Device->BeginScene() >= 0)
+	{
+		ImGui::Render();
+		ImGui_ImplDX9_RenderDrawData(ImGui::GetDrawData());
+		p_Device->EndScene();
+	}
+
+	HRESULT result = p_Device->Present(NULL, NULL, NULL, NULL);
+
+	if (result == D3DERR_DEVICELOST && p_Device->TestCooperativeLevel() == D3DERR_DEVICENOTRESET)
+	{
+		ImGui_ImplDX9_InvalidateDeviceObjects();
+		p_Device->Reset(&d3dpp);
+		ImGui_ImplDX9_CreateDeviceObjects();
+	}
+}
+
+auto main_loop() -> WPARAM
+{
+	static RECT old_rc;
+	ZeroMemory(&Message, sizeof(MSG));
+
+	while (Message.message != WM_QUIT)
+	{
+		if (PeekMessage(&Message, MyWnd, 0, 0, PM_REMOVE)) {
+			TranslateMessage(&Message);
+			DispatchMessage(&Message);
+		}
+
+		HWND hwnd_active = GetForegroundWindow();
+		if (GetAsyncKeyState(0x23) & 1)
+			exit(8);
+
+		if (hwnd_active == GameWnd) {
+			HWND hwndtest = GetWindow(hwnd_active, GW_HWNDPREV);
+			SetWindowPos(MyWnd, hwndtest, 0, 0, 0, 0, SWP_NOMOVE | SWP_NOSIZE);
+		}
+		RECT rc;
+		POINT xy;
+
+		ZeroMemory(&rc, sizeof(RECT));
+		ZeroMemory(&xy, sizeof(POINT));
+		GetClientRect(GameWnd, &rc);
+		ClientToScreen(GameWnd, &xy);
+		rc.left = xy.x;
+		rc.top = xy.y;
+
+		ImGuiIO& io = ImGui::GetIO();
+		io.ImeWindowHandle = GameWnd;
+		io.DeltaTime = 1.0f / 60.0f;
+
+		POINT p;
+		GetCursorPos(&p);
+		io.MousePos.x = p.x - xy.x;
+		io.MousePos.y = p.y - xy.y;
+
+		if (GetAsyncKeyState(0x1)) {
+			io.MouseDown[0] = true;
+			io.MouseClicked[0] = true;
+			io.MouseClickedPos[0].x = io.MousePos.x;
+			io.MouseClickedPos[0].x = io.MousePos.y;
+		}
+		else io.MouseDown[0] = false;
+
+		if (rc.left != old_rc.left || rc.right != old_rc.right || rc.top != old_rc.top || rc.bottom != old_rc.bottom) {
+
+			old_rc = rc;
+
+			Width = rc.right;
+			Height = rc.bottom;
+
+			p_Params.BackBufferWidth = Width;
+			p_Params.BackBufferHeight = Height;
+			SetWindowPos(MyWnd, (HWND)0, xy.x, xy.y, Width, Height, SWP_NOREDRAW);
+			p_Device->Reset(&p_Params);
+		}
+		render();
+	}
+
+	ImGui_ImplDX9_Shutdown();
+	ImGui_ImplWin32_Shutdown();
+	ImGui::DestroyContext();
+
+	cleanup_d3d();
+	DestroyWindow(MyWnd);
+
+	return Message.wParam;
+}
